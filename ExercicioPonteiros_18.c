3a134913f613bac41743f5b9c68fc351073a76ba
JoaoPedroRsilva <jprdsilva@inf.ufpel.edu.br>
2025-05-05 23:02:01 -0300

    Editei a formataçao da funçao.

--- ExercicioPonteiros_18.c
+++ ExercicioPonteiros_18.c
@@ -2,7 +2,7 @@
 
 #define PI 3,141592653589
 
-void calc_esfera(float R, float *area, float *volume);
+void calc_esfera ( float R, float *area, float *volume );
 
 int main ()
 {
@@ -10,7 +10,7 @@ int main ()
     float volume = 0;
     float raio = 3.5f;
 
-    calc_esfera(raio, &area, &volume);
+    calc_esfera ( raio, &area, &volume );
 
     printf("\tRaio da esfera: %.2f\n", raio);
     printf("\tArea da Superficie: %.2f\n", area);
@@ -19,7 +19,7 @@ int main ()
     return 0;
 }
 
-void calc_esfera(float R, float *area, float *volume)
+void calc_esfera ( float R, float *area, float *volume )
 {
     *area = 4 * PI * ( R * R );
     *volume = ( 4 / 3 ) * PI * ( R * R * R );
